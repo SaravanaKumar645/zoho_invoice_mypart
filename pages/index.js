70a194e7e138963c5928c0aa15e504bab5fb8451
SARAV <sarav023b@gmail.com>
2021-10-13 23:53:24 +0530

    zoho invoice -landing page footer ui

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Footer from "../Components/Footer";
+import Otherapps from "../Components/Otherapps";
+import Invoicedetails from "../Components/Invoicedetails";
+export default function Home() {
+  return (
+    <div>
+      <Invoicedetails />
+      <Otherapps />
+      <Footer />
+    </div>
+  );
+}
